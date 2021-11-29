b308540a07644d3f2731118efe2d3cbc65135326
gabriella garcia <gabirvgarcia@hotmail.com>
2021-11-28 22:14:44 -0300

    trying to do dropdown

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,6 +3,8 @@ class Game < ApplicationRecord
   has_many :games_list_games
   has_many :games_list, through: :games_list_games
 
+  accepts_nested_attributes_for :games_list_games, reject_if: :all_blank, allow_destroy: true
+
   validates :name, :summary, :category, presence: true
   validates :name, length: { minimum: 3 }
   validates :name, uniqueness: true
--- app/models/games_list.rb
+++ app/models/games_list.rb
@@ -3,5 +3,8 @@ class GamesList < ApplicationRecord
   has_many :games_list_games, dependent: :destroy
   has_many :games, through: :games_list_games
 
+  accepts_nested_attributes_for :games_list_games, reject_if: :all_blank, allow_destroy: true
+  accepts_nested_attributes_for :games, reject_if: :all_blank, allow_destroy: true
+
   has_one_attached :photo
 end
